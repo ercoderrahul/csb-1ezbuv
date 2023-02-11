c30a5683dc8e6e2c8f74017b8643895f29df58a5
Code Sandbox <hello@codesandbox.io>
2023-02-11 17:16:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f0cd7dcd2084d9ae331934a85f943157.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9650c5b98f0339c14df083ee64e8b16c",
+    "url": "https://ercoderrahul.github.io/csb-1ezbuv/index.html"
+  },
+  {
+    "revision": "23236192608c0691864e",
+    "url": "https://ercoderrahul.github.io/csb-1ezbuv/static/js/main.b79df2c9.chunk.js"
+  },
+  {
+    "revision": "8f3002ceb5e929c479f3",
+    "url": "https://ercoderrahul.github.io/csb-1ezbuv/static/js/runtime-main.89b81221.js"
+  }
+]);
